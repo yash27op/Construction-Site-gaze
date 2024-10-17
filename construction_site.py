8645530c28681202185c6b7f6d827d1f9302de85
yash27op <118678900+yash27op@users.noreply.github.com>
2024-10-17 14:34:36 +0530

    Update construction_site.py

--- construction_site.py
+++ construction_site.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
-
+#ssc
 # Sample Data
 data = {
     'Project': ['Site A', 'Site B', 'Site C', 'Site D'],
